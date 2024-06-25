5c86842a2685cdf5ab0b506c8c63314198a1b784
Lizbeth de Jesús Bernal Franco <230110346@itsoeh.edu.mx>
2024-06-25 12:11:49 -0600

    Se modifico un archivo de la carpeta CodeBase 2

--- CodeBase/plot_hist.py
+++ CodeBase/plot_hist.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-def generar_histograma(frecuencia_relativa, frecuencia, clases_sorted, labelx, labely, titulo):
+def generar_histograma(marcas_clases, frecuencia, clases_sorted, labelx, labely, titulo):
     """
     Genera un histograma con las características especificadas.
 
@@ -15,11 +15,11 @@ def generar_histograma(frecuencia_relativa, frecuencia, clases_sorted, labelx, l
     plt.figure(figsize=(12, 6))
 
     # Histograma con barras ajustadas al 100%, contorno negro y colores específicos
-    plt.bar(frecuencia_relativa, frecuencia, width=1, edgecolor="k",
+    plt.bar(marcas_clases, frecuencia, width=1, edgecolor="k",
             color=["#FF5733", "#33FFF3", "#FF33F9", "#3339FF", "#E6FF33", "#FF3346"])
 
     # Personalizar marcas del eje X (inclinadas 45°) y tamaño de fuente
-    plt.xticks(frecuencia_relativa, clases_sorted, fontsize=15, rotation=45)
+    plt.xticks(marcas_clases, clases_sorted, fontsize=15, rotation=45)
 
     # Etiquetas y título del gráfico con tamaño de fuente grande
     plt.xlabel(labelx, fontsize=25)
